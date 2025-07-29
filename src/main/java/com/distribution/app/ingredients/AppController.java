faa69f00493a04216efe12d471f7cdbd73a5d426
mireu930 <mireu930@github.com>
2025-07-29 20:40:51 +0900

    DB수정

--- src/main/java/com/distribution/app/ingredients/AppController.java
+++ src/main/java/com/distribution/app/ingredients/AppController.java
@@ -1,14 +1,18 @@
 package com.distribution.app.ingredients;
 
+import java.util.UUID;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 	
+	private final String uniqueId = UUID.randomUUID().toString();
+	
 
 	@GetMapping("/health")
 	public String healthCheck() {
-		return "success health check";
+		return "Server ID: " + uniqueId;
 	}
 }
