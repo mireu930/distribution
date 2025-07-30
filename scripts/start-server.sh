7d9bb27c4c0aa0ae5ec38db11ebfc10419725e3f
mireu930 <mireu930@github.com>
2025-07-30 20:35:30 +0900

    경로수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -3,5 +3,5 @@
 echo "--------------- 서버 배포 시작 -----------------"
 cd /home/ubuntu/distribution
 sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
+nohup java -jar project.jar > /home/ubuntu/output.log 2>&1 &
 echo "--------------- 서버 배포 끝 -----------------"
