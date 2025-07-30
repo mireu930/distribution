d995bbaee6d9d9b7e38359471e94328730daf88e
mireu930 <mireu930@github.com>
2025-07-30 13:32:47 +0900

    home 수정

--- src/main/java/com/distribution/app/home/HomeController.java
+++ src/main/java/com/distribution/app/home/HomeController.java
@@ -2,12 +2,13 @@ package com.distribution.app.home;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
 	@GetMapping("/")
 	public String home() throws Exception {
-		return "index";
+		return "hi";
 	}
 }
