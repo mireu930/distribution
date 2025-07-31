71a7aa03b351d18856e7c1599ff4ae34c746a926
mireu930 <mireu930@github.com>
2025-07-31 15:30:12 +0900

    반영 test

--- my_app/src/layout/topbar.jsx
+++ my_app/src/layout/topbar.jsx
@@ -1,7 +1,7 @@
 export default function Topbar() {
     return(
         <header className="bg-dark text-white py-3">
-        <h1>물류창고 관리 시스템</h1>
+        <h1>물류창고 관리 시스템ㅎㅎ</h1>
       </header>
     )
 }
