a5b71f91c09c7f1e978503add8ff5bceeca7f731
mireu930 <mireu930@github.com>
2025-08-18 23:23:39 +0900

    ecr적용

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/distribution
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > /home/ubuntu/output.log 2>&1 &
+docker stop distribution || true
+docker rm distribution || true
+docker pull 221082169763.dkr.ecr.ap-northeast-2.amazonaws.com/distribution:latest
+docker run -d --name distribution -p 8080:8080 221082169763.dkr.ecr.ap-northeast-2.amazonaws.com/distribution
 echo "--------------- 서버 배포 끝 -----------------"
